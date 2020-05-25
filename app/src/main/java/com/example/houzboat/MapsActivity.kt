732ee6c718d3b5d9ef4b42e80c84369c40f00344
João Paulo de Oliveira <jpos.usp@gmail.com>
2020-05-25 00:50:31 -0300

    Change Sydney latlng position

--- app/src/main/java/com/example/houzboat/MapsActivity.kt
+++ app/src/main/java/com/example/houzboat/MapsActivity.kt
@@ -36,8 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(-35.5, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker is Sidney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
